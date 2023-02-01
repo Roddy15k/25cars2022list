7f50e086e2e3bc6bf5f044f4f420fc30038fea9e
Roddy15k <rxs2907@gmail.com>
2023-01-31 18:17:30 -0600

    clean up app

---
+++ src/components/Dialog.js
@@ -0,0 +1,12 @@
+const Dialog = () => {
+  return (
+    <div className="dialog-box">
+      <h1>Top 25 cars sold in 2022</h1>
+      <h2>
+        We will review thhe best selling vehicles of 2022. Some vehicles that
+        made the list will be surprizing. Others will be as expected.
+      </h2>
+    </div>
+  )
+}
+export default Dialog

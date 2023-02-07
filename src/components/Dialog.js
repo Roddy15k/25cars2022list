6570592ac8f436db24eed5ff545f9f2e81940290
Roddy15k <rxs2907@gmail.com>
2023-02-06 18:03:06 -0600

    21

--- src/components/Dialog.js
+++ src/components/Dialog.js
@@ -3,7 +3,7 @@ const Dialog = () => {
     <div className="dialog-box">
       <h1>Top 25 cars sold in 2022</h1>
       <h2>
-        We will review thhe best selling vehicles of 2022. Some vehicles that
+        We will review the best selling vehicles of 2022. Some vehicles that
         made the list will be surprising.
       </h2>
     </div>
